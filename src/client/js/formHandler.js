580328d57810d86b44c1544644803a874c3c43dd
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-07-04 19:54:39 -0400

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -14,3 +14,22 @@ function handleSubmit(event) {
 }
 
 export { handleSubmit }
+
+const formdata = new FormData();
+formdata.append("key", "0bba2cb598ad236996dab260dbb69b61");
+formdata.append("txt", "YOUR TEXT HERE");
+formdata.append("lang", "TEXT LANGUAGE HERE");  // 2-letter code, like en es fr ...
+
+const requestOptions = {
+  method: 'POST',
+  body: formdata,
+  redirect: 'follow'
+};
+
+const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+  .then(response => ({
+    status: response.status, 
+    body: response.json()
+  }))
+  .then(({ status, body }) => console.log(status, body))
+  .catch(error => console.log('error', error));
