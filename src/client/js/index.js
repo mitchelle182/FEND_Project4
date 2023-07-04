8ae74cfdba8482dee5f2428591fbfccc2b8d7cb8
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-07-04 18:44:13 -0400

    Add files via upload

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,10 +1,10 @@
 import { handleSubmit } from "./formHandler";
 import { checkForName } from "./nameChecker";
 
-import './styles/resets.scss';
-import './styles/base.scss';
-import './styles/footer.scss';
-import './styles/form.scss';
-import './styles/header.scss';
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
 
 alert ("I EXIST!!!");
