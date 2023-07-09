29772a0533421cbb025b603d45f082a7a49f3b28
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-07-09 17:57:40 -0400

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,32 +1,28 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formText = document.getElementById('name').value;
+    Client.checkForName(formText);
 
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8080/test')
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+        document.getElementById('results').innerHTML = res.message;
+    });
 }
 
 export { handleSubmit }
 
 const formdata = new FormData();
 formdata.append("key", "0bba2cb598ad236996dab260dbb69b61");
-formdata.append("txt", "YOUR TEXT HERE");
+formdata.append("url", "YOUR TEXT HERE");
 formdata.append("lang", "TEXT LANGUAGE HERE");  // 2-letter code, like en es fr ...
 
-const requestOptions = {
-  method: 'POST',
-  body: formdata,
-  redirect: 'follow'
-};
 
-const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+
+const response = fetch("https://api.meaningcloud.com/sentiment-2.1?key={{YOUR_API_KEY}}&lang=en&url=https://www.udacity.com/blog/2023/05/getting-started-with-r-markdown.html")
   .then(response => ({
     status: response.status, 
     body: response.json()
