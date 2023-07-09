0b7a7cc19f15dfe2c076a9b4574e598ba4644738
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-07-09 17:55:08 -0400

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const mockAPIResponse = require('./mockAPI.js');
 
-const meaningCloud = require('meaningCloud_textapi');
-let meaningCloud_textapi = new meaningCloud ({
-    application_key:"0bba2cb598ad236996dab260dbb69b61"
-})
-const app = express()
 
-app.use(express.static('dist'))
+const app = express();
+
+app.use(express.static('dist'));
 
 console.log(__dirname)
 
@@ -20,10 +17,10 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!');
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
-})
+});
