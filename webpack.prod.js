68d139c76bb6a7a51312afc8944a11820c93289c
MichelleT <1564793+mitchelle182@users.noreply.github.com>
2023-07-09 17:53:50 -0400

    Add files via upload

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,29 +1,43 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({})]
+    },
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin, 'css-loader', 'sass-loader']
             }
         ],
+        
         output: {
             libraryTarget: 'var',
             library: 'Client'
         },
+        
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new WorkboxPlugin.GenerateSW()
+        new WorkboxPlugin.GenerateSW(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
     ]
-}
+};
